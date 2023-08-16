6875bdd305b8508cace53ec316e9d1a1b6071219
Graham Trogdon <graham.trogdon@gmail.com>
2023-08-16 00:09:12 -0400

    added TestScene and deleted SampleScene

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Internal;
+
+public class Paralax : MonoBehaviour {
+
+	public Camera _mainview;//if the camera you use is not Camera.main you need to fix it here in Start()!
+	public Vector3 _focusposition;
+	private Vector3 _startposition;
+	private Vector3 _camstartposition;
+	public int _paralaxshift;
+
+	// Use this for initialization
+	void Start () {
+		_mainview = Camera.main;
+		_startposition = gameObject.transform.position;
+		_camstartposition = _mainview.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//divide camera location by shift
+		float camcomp = (_mainview.gameObject.transform.position.x - _focusposition.x) / _paralaxshift;
+		float selfcomp = _startposition.x - gameObject.transform.position.x;
+
+		//if gameobject position is not equal to start position + shift result change position
+		if (Mathf.Abs (camcomp) != Mathf.Abs (selfcomp)) {
+			float hold = camcomp * 10;
+			hold = Mathf.Round (hold);
+			camcomp = hold / 10;
+			gameObject.transform.position = _startposition + new Vector3 (camcomp, 0, 0);
+			//Debug.Log (gameObject.transform.position.x);
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
-    [SerializeField] private float _gravity;
-    [SerializeField] private float _playerSpeed = 5.0f;
+	private Animator _animator;
+    [SerializeField] private float _gravityModifier;
+    [SerializeField] private float _playerSpeed = 3.5f;
     [SerializeField] private float _jumpHeight;
+
+    // Singleton Instantiation
+    private static Player instance;
+    public static Player Instance {
+        get {
+            if (instance == null) instance = GameObject.FindObjectOfType<Player>();
+            return instance;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();        
+        _animator = GetComponentInChildren<Animator>();        
     }
 
     // Update is called once per frame
     void Update()
     {
         float velocityX = Input.GetAxis("Horizontal");
-        float velocityZ = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(velocityX, 0, velocityZ) +
-            (_controller.isGrounded ? Vector3.zero : Physics.gravity);
+        Vector3 move = new Vector3(velocityX, 0, 0) +
+            (_controller.isGrounded ? Vector3.zero : Physics.gravity * _gravityModifier);
         _controller.Move(move * _playerSpeed * Time.deltaTime);
+        _animator.SetBool("IsMoving", velocityX != 0 ? true : false);
+        _animator.SetFloat("Movement", velocityX);          // allow animator state machine access to value
     }
 }
