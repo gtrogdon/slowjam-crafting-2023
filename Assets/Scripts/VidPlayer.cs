12974fec11d71099b2de6517c4a7c004e7bf5881
Ariane Joy Nova <arianenova@gmail.com>
2023-08-25 18:34:29 -0400

    webgl fix for title screen video

---
+++ Assets/Scripts/VidPlayer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VidPlayer : MonoBehaviour
+{
+
+    [SerializeField] string videoFileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayVideo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log("videoPath: " + videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+}
