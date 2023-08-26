7240aab853e07e745a7622ac8109ecfbbc43eada
PoggerLro <77075171+PoggerLro@users.noreply.github.com>
2023-08-26 17:45:06 +0900

    Fmod Test
    Added Fmod

---
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using FMODUnity;
+using System.CodeDom;
+
+public class FMODEvents : MonoBehaviour
+{
+
+    [field: Header("Title Music")]
+    [field:SerializeField] public EventReference titleMusic { get; private set; }
+
+
+    [field: Header("Footsteps SFX")]
+
+    [field:SerializeField] public EventReference playerFootsteps { get; private set; }
+
+    [field: Header("Pickup SFX")]
+
+    [field: SerializeField] public EventReference itemPickup { get; private set; }
+
+
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            UnityEngine.Debug.LogError("Found more than one FMOD Events instance in the scene.");
+        }
+        instance = this;
+    }
+}
